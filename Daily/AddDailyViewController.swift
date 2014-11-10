4dd1ba5aed18687d7103bbf5f3d872318d95a75e
Thomas Meagher <thom.meagher@gmail.com>
2014-11-09 22:12:55 -0500

    UISegmentedControl to update section

--- Daily/AddDailyViewController.swift
+++ Daily/AddDailyViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class AddDailyViewController: UIViewController {
 
+    @IBOutlet weak var typeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var nameTextField: UITextField!
     
     var mainVC: ViewController!
+    
+    var type = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +29,29 @@ class AddDailyViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    
+    @IBAction func typeSegmentedControlChanged(sender: UISegmentedControl) {
+        if self.typeSegmentedControl.selectedSegmentIndex == 0 {
+            type = true
+        }
+        else if self.typeSegmentedControl.selectedSegmentIndex == 1 {
+            type = false
+        }
+        else {
+            type = true
+        }
+    }
+    
     @IBAction func doneButtonTapped(sender: UIButton) {
-        var newDaily = DailyModel(name: nameTextField.text, type: true)
-        mainVC?.baseArray[0].append(newDaily)
+        var newDaily = DailyModel(name: nameTextField.text, type: self.type)
+        
+        if newDaily.type == true {
+            mainVC?.baseArray[0].append(newDaily)
+        }
+        else {
+            mainVC?.baseArray[1].append(newDaily)
+        }
+        
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 }
--- Daily/DailyDetailViewController.swift
+++ Daily/DailyDetailViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class DailyDetailViewController: UIViewController {
 
+    @IBOutlet weak var typeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var nameTextField: UITextField!
     
     var mainVC: ViewController!
     
     var detailDailyModel: DailyModel!
     
+    var type = true
+    
+    var reverseType = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if self.detailDailyModel.type == true {
+            reverseType = 0
+        }
+        else if self.detailDailyModel.type == false {
+            reverseType = 1
+        }
+        else {
+            reverseType = 0
+        }
+        
+        self.typeSegmentedControl.selectedSegmentIndex = reverseType
         self.nameTextField.text = detailDailyModel.name
     }
 
@@ -26,10 +42,29 @@ class DailyDetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    
+    @IBAction func typeSegmentedControlChanged(sender: UISegmentedControl) {
+        if self.typeSegmentedControl.selectedSegmentIndex == 0 {
+            type = true
+        }
+        else if self.typeSegmentedControl.selectedSegmentIndex == 1 {
+            type = false
+        }
+        else {
+            type = true
+        }
+    }
+    
     @IBAction func doneBarButtonItemTapped(sender: UIBarButtonItem) {
-        var daily = DailyModel(name: nameTextField.text, type: true)
+        var daily = DailyModel(name: nameTextField.text, type: self.type)
         
-        self.mainVC.baseArray[0][mainVC.tableView.indexPathForSelectedRow()!.row] = daily
+        // If daily already exists in baseArray[0], then replace row. If it doesn't append it.
+        if daily.type == true {
+            self.mainVC.baseArray[0][mainVC.tableView.indexPathForSelectedRow()!.row] = daily
+        }
+        else {
+            self.mainVC.baseArray[1][mainVC.tableView.indexPathForSelectedRow()!.row] = daily
+        }
         
         self.navigationController?.popViewControllerAnimated(true)
     }
